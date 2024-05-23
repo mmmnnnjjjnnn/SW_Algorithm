9a9d6813e50b30ea50db05230332875de278b37d
taesung1.park <taesung1.park@lge.com>
2024-05-23 17:04:59 +0900

    Update week3

--- week3/ts_14940.py
+++ week3/ts_14940.py
@@ -2,15 +2,15 @@ from collections import deque
 
 n, m = map(int, input().split())
 graph = []
-for _ in range(m):
+for _ in range(n):
     graph.append(list(map(int, input().split())))
 
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
 r, c = 0, 0
-for i in range(m):
-    for j in range(n):
+for i in range(n):
+    for j in range(m):
         if graph[i][j] == 2:
             r, c = i, j
             graph[r][c] = 0
@@ -18,8 +18,8 @@ for i in range(m):
 queue = deque([(r, c)])
 
 visited = []
-for _ in range(m):
-    visited.append([False for _ in range(n)])
+for _ in range(n):
+    visited.append([False for _ in range(m)])
 
 while queue:
     x, y = queue.popleft()
@@ -27,7 +27,7 @@ while queue:
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if nx < 0 or nx >= m or ny < 0 or ny >= n:
+        if nx < 0 or nx >= n or ny < 0 or ny >= m:
             continue
 
         if graph[nx][ny] == 1 and visited[nx][ny] is False:
@@ -35,8 +35,8 @@ while queue:
             queue.append((nx, ny))
             visited[nx][ny] = True
 
-for i in range(m):
-    for j in range(n):
+for i in range(n):
+    for j in range(m):
         if graph[i][j] == 1 and visited[i][j] is False:
             graph[i][j] = -1
         print(graph[i][j], end=' ')
